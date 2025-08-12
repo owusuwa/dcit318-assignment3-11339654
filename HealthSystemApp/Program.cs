8db56347a0b0492040b031e935c8d2e6be3c63be
Nana <nana@gmail.com>
2025-08-12 06:07:40 +0000

    Added Patient and Prescription classes for Health System App

---
+++ HealthSystemApp/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Patient
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+
+    public Patient(int id, string name, int age, string gender)
+    {
+        Id = id;
+        Name = name;
+        Age = age;
+        Gender = gender;
+    }
+}
+
+public class Prescription
+{
+    public int Id { get; set; }
+    public int PatientId { get; set; }
+    public string MedicationName { get; set; }
+    public DateTime DateIssued { get; set; }
+
+    public Prescription(int id, int patientId, string medicationName, DateTime dateIssued)
+    {
+        Id = id;
+        PatientId = patientId;
+        MedicationName = medicationName;
+        DateIssued = dateIssued;
+    }
+}
