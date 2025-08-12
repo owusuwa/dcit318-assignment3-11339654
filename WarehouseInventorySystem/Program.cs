a3f7f10c872491a9c23c8700921ed5f42ecad056
Nana <nana@gmail.com>
2025-08-12 08:53:30 +0000

    Added generic Repository<T> with custom exception for Q3

--- WarehouseInventorySystem/Program.cs
+++ WarehouseInventorySystem/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Custom exception for when an item is not found
+public class ItemNotFoundException : Exception
+{
+    public ItemNotFoundException(string message) : base(message) { }
+}
+
+// Generic repository with basic CRUD
+public class Repository<T>
+{
+    private List<T> items = new List<T>();
+
+    public void Add(T item)
+    {
+        items.Add(item);
+    }
+
+    public List<T> GetAll()
+    {
+        return items;
+    }
+
+    public T Get(Func<T, bool> predicate)
+    {
+        var item = items.FirstOrDefault(predicate);
+        if (item == null)
+            throw new ItemNotFoundException("Item not found in repository.");
+        return item;
+    }
+
+    public bool Remove(Func<T, bool> predicate)
+    {
+        var item = items.FirstOrDefault(predicate);
+        if (item != null)
+        {
+            items.Remove(item);
+            return true;
+        }
+        throw new ItemNotFoundException("Cannot remove: Item not found in repository.");
+    }
+}
